a7202118b4e9af5032d7ee125e42b6767fc7dabe
webdev-mamun <mamunur.m@gmail.com>
2023-07-15 04:58:13 +0600

    add font on tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
-  content: [],
+  content: [
+    './config/*.json',
+    './layout/*.liquid',
+    './assets/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid',
+    './templates/*.json',
+    './templates/customers/*.liquid',
+    './locales/*.json',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'sans': ['Tomato Grotesk', ...defaultTheme.fontFamily.sans],
+        'serif': ['TT Ramillas', ...defaultTheme.fontFamily.serif],
+      },
+    },
   },
   plugins: [],
 }
